879bb143f9ba218ec0cd2e41f44654128aa80a70
maxwel <maximusekeh@gmail.com>
2021-05-04 20:49:32 +0100

    new changes

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const openMenu = document.querySelector('.openMenu');
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
+
 function show() {
-    navLinks.style.display = 'flex';
-    navLinks.style.top = '0';
+    navLinks.classList.remove('hide');
+    navLinks.classList.add('show');
+    closeMenu.style.display= 'block'
+    openMenu.style.display = 'none'
+    // navLinks.style.top = '0';
 }
 
 function close() {
-    navLinks.style.top = '-100%';
+    navLinks.classList.remove('show');
+    navLinks.classList.add('hide');
+    closeMenu.style.display= 'none'
+    openMenu.style.display = 'block'
 }
